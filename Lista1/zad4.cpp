975a0b39c44969131e2f06fdab7f36dc9b52702b
Klimzaj <klimzaj@gmail.com>
2019-03-02 10:30:06 +0100

    exercise 4

---
+++ Lista1/zad4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+double fac(double n)
+{
+    if(n > 1)
+        return n * fac(n - 1);
+    else
+        return 1;
+}
+double P(double k,double n,double p)
+{
+    double wynik = 0.0;
+    for(int i = 1;i<k;i++)
+    {
+        wynik += fac(n)/(fac(k)*fac(std::abs(n-k)))*pow(p,i)*pow(p,n-i); //dowiedziec sie o "q"
+        // std::cout<<wynik<<std::endl;
+    }
+    return wynik;
+}
+
+int main()
+{
+    std::cout<<P(5,2,2)<<std::endl;
+    // std::cout<<fac(3)<<std::endl;
+    return 0;
+    
+}
