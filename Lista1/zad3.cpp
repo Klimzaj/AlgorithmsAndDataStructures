249e881be2ddebb57aa0e7353e7e4ff8bf8e70fa
Klimzaj <klimzaj@gmail.com>
2019-03-05 20:23:45 +0100

    fix exercise 3

--- Lista1/zad3.cpp
+++ Lista1/zad3.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
+#include <cmath>
 
 double oblicz(double a[], int n,double x)
 {
-    int wynik = a[0];
+    double wynik = 0.0;
+    int iloscMnozen = 0;
 
-	for(int i=1;i<=n;i++)
-		wynik = wynik*x + a[i];
-	
-	return wynik;
+    int p = n;
+
+	for(int i=0;i < n;i++)
+    {
+        wynik += a[i] * std::pow(x,p--);
+
+        iloscMnozen++;
+        iloscMnozen += p-1;
+    }
+
+    std::cout << "Dla n= "<< n << " wykonamy " << iloscMnozen << " mnożeń." <<std::endl;
+	return wynik += a[n];
 }
+
+// zlozonosc algortymu to: 2n + 3
 
 int main()
 {
-    double _a[10] = {4,5,3,2,2,3,4,8,0,9};
-    int _n = 7;
-    double _x = 2.4;
-    std::cout<<oblicz(_a,_n,_x)<<std::endl;
+    int _n = 10;
+    // double _a[_n] = {4,5,3,2,2,3,4,8,0,9};
+    double _b[4] = {2,1,3,4};
+    double _x = 2;
+    std::cout << oblicz(_b,4,_x) << std::endl;
     return 0;
 
 }
