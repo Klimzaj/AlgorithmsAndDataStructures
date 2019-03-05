63dc7f96e049a7a98fc61e8ef1032b9ab19a9bf8
Klimzaj <klimzaj@gmail.com>
2019-03-05 10:31:53 +0100

    exercise 4

---
+++ Lista2/zad4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std; 
+
+void wypisz(int t[], int n)
+{
+    for(int i = 0; i<n;i++)
+        cout<<t[i]<<" ";
+}
+
+void przesuniecie(int t[], int k, int n) {
+    for (int j = 0; j < k; j++) {
+        int temp = t[n - 1];
+        int temp1;
+        for (int i = 0; i < n; i++) {
+            temp1 = t[i];
+            t[i] = temp;
+            temp = temp1;
+        }
+    }
+}
+int main() {
+
+    int N = 10;
+    int tab[N] = {0,1,2,3,4,5,6,7,8,9};
+    
+    wypisz(tab,N);
+    cout<<endl;
+    przesuniecie(tab, 4, N);
+    cout<<endl;
+    wypisz(tab,N);
+    
+
+    return 0;
+}
