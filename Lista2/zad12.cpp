44a15a648f345fc777caaf3041ddda59c9e5efad
Kuba <kuba@MacBook-Pro-Kuba.local>
2019-03-11 17:08:58 +0100

    exercise 12

---
+++ Lista2/zad12.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int x;
+    node* left;
+    node* right;
+    node* parent;
+    node(int _val, node* _l=NULL, node* _r=NULL, node* _p=NULL)
+        :x(_val),left(_l),right(_r),parent(_p)
+    {}
+};
+
+void insert(node *& _t, int _x)
+{
+    node **temp = &_t;
+    while(*temp)
+    {
+		if((*temp)->x>_x)
+		{
+			temp = &((*temp)->left);
+		}
+		else temp = &((*temp)->right);
+    }
+    node *d = *temp;
+    *temp = new node(_x);
+    (*temp)->parent = d;
+}
+
+void remove(node *& _t, int _x)
+{
+    node **temp = &_t;
+    node **parent = &(*temp)->parent;
+
+    while(*temp)
+    {
+        parent = temp; 
+		if((*temp)->x>_x)
+		{
+			temp = &((*temp)->left);
+		}
+		else temp = &((*temp)->right);
+    }
+
+    if(*temp)
+    {
+        if((*temp)->left && (*temp)->right)
+        {
+            node **delta=&((*temp)->right);
+			while ((*delta)->left)
+				delta=&((*delta)->left);
+			(*temp)->x=(*delta)->x;
+			temp=delta;
+        }
+        else if((*temp)->left == NULL && (*temp)->right == NULL)
+        {
+            if((*parent)->left->x == _x)
+            {
+                (*parent)->left = NULL;
+            }
+            else
+            {
+                 (*parent)->right = NULL;
+            }
+            delete *
+            
+            // if((*temp)->parent)
+            // (*temp)->parent->
+        }
+        else if((*temp)->left == NULL)
+        {
+            node* delta = (*temp)->right;
+			delete *temp;
+			(*temp) = delta;
+        }
+        else
+        {
+            node* delta = (*temp)->left;
+			delete *temp;
+			(*temp) = delta;
+        }
+        
+        
+    }
+
+}
+int main()
+{
+    node* root = NULL;
+    insert
+
+    return 0;
+}
