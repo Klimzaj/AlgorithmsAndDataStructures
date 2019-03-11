2bae1904587379d801a0f73389f0c3f56b2cc6b0
Kuba <kuba@MacBook-Pro-Kuba.local>
2019-03-11 17:08:37 +0100

    exercise 11

---
+++ Lista2/zad11.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int x;
+    node* left;
+    node* right;
+
+    node(int val, node* l=NULL, node* r=NULL)
+		:x(val), left(l), right(r)
+	{}
+};
+
+void remove(node *& _t, int _x)
+{
+	node** delta = &_t;
+    node** delta2 = &_t;
+	while(*delta && (*delta)->x!=_x)
+	{
+        delta2 = delta;
+		if((*delta)->x>_x)
+		{
+			delta = &((*delta)->left);
+		}
+		else delta = &((*delta)->right);
+	}
+	if(*delta)
+	{
+		if((*delta)->left && (*delta)->right)
+		{
+			node **temp=&((*delta)->right);
+			while ((*temp)->left)
+				temp=&((*temp)->left);
+			(*delta)->x=(*temp)->x;
+			delta=temp;
+		}
+        else if((*delta)->left == NULL && (*delta)->right == NULL)
+        {
+            if((*delta2)->left->x == _x)
+            {
+                (*delta2)->left = NULL;
+            }
+            else
+            {
+                (*delta2)->right = NULL;
+            }
+            delete *delta;
+            
+        }
+		else if((*delta)->left == NULL)
+		{
+			node* temp = (*delta)->right;
+			delete *delta;
+			(*delta) = temp;
+		}
+		else
+		// else if((*delta)->right == NULL)
+		{
+			node* temp = (*delta)->left;
+			delete *delta;
+			(*delta) = temp;
+		}	
+	}
+}
+
+int main()
+{
+
+    return 0;
+}
