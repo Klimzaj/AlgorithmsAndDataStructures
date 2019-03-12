9345bd2fb5d08a2e8350faf2c0623bb437b86a1c
Kuba <kuba@MacBook-Pro-Kuba.local>
2019-03-12 19:32:38 +0100

    fix exercise 12

--- Lista2/zad12.cpp
+++ Lista2/zad12.cpp
@@ -32,7 +32,7 @@ void insert(node *& _t, int _x)
 void remove(node *& _t, int _x)
 {
     node **temp = &_t;
-    node **parent = &(*temp)->parent;
+    node **parent = &((*temp)->parent);
 
     while(*temp)
     {
@@ -43,23 +43,24 @@ void remove(node *& _t, int _x)
 		}
 		else temp = &((*temp)->right);
     }
+    // cout<< (*temp)->x<<endl;
 
     if(*temp)
     {
-        if((*delta)->left && (*delta)->right)
+        if((*temp)->left && (*temp)->right)
 		{
-			node **temp=&((*delta)->left);
-			temp=&((*temp)->left);
+			node **delta=&((*temp)->left);
+			delta=&((*delta)->left);
 
-			while ((*temp)->right)
-				temp=&((*temp)->right);
+			while ((*delta)->right)
+				delta=&((*delta)->right);
 
-			(*delta)->x=(*temp)->x;
-			(*delta)->left=(*temp)->left;
-			(*delta)->right=(*temp)->right;
+			(*temp)->x=(*delta)->x;
+			(*temp)->left=(*delta)->left;
+			(*temp)->right=(*delta)->right;
 
 			// delta=temp;
-			delete temp;
+			delete *delta;
 		}
         // if((*temp)->left && (*temp)->right)
         // {
@@ -98,9 +99,102 @@ void remove(node *& _t, int _x)
     }
 
 }
+
+void remove2(node *& _t, int _x)
+{
+	node** delta = &_t;
+    node** parent = &((*delta)->parent);
+
+	while(*delta && (*delta)->x!=_x)
+	{
+        parent = delta;
+		if((*delta)->x>_x)
+		{
+			delta = &((*delta)->left);
+		}
+		else delta = &((*delta)->right);
+	}
+	if(*delta)
+	{
+		cout << "test" << endl;
+		if((*delta)->left && (*delta)->right)
+		{
+			cout<<"Dwie galezie zajete"<<endl;
+
+			node **temp=&((*delta)->left);
+			// cout <<(*temp)<<endl;
+			temp=&((*temp)->left);
+
+			while ((*temp)->right != NULL)
+				temp=&((*temp)->right);
+
+			// cout<<"po while, temp: "<<(*temp)->x<<" delta: "<<(*delta)->x<<endl;
+
+			(*delta)->x=(*temp)->x;
+
+			// cout << (*delta)->x <<endl;
+
+			if((*delta)->left != NULL)
+			{
+				(*delta)->left=(*temp)->left;
+			}
+            else if((*delta)->right != NULL)
+            {
+				(*delta)->right=(*temp)->right;
+            }
+
+
+			// delta=temp;
+			delete *temp;
+		}
+		//dziala
+        else if((*delta)->left == NULL && (*delta)->right == NULL)
+        {
+			cout<< "right&left = NULL"<< endl;
+			// if((*delta2)->left!=NULL ? (*delta)->x == _x)
+
+			if((*parent)->left != NULL && (*parent)->left->x == _x)
+			{
+				(*parent)->left = NULL;
+			}
+			else if((*parent)->right != NULL && (*parent)->right->x == _x)
+			{
+				(*parent)->right = NULL;
+			}
+
+            delete *delta;
+        }
+		// dziala
+		else if((*delta)->left == NULL)
+		{
+			cout<< "left = NULL"<< endl;
+			node* temp = (*delta)->right;
+			delete *delta;
+			(*delta) = temp;
+		}
+		// dziala
+		else
+		{
+			cout<< "right = NULL"<< endl;
+			node* temp = (*delta)->left;
+			delete *delta;
+			(*delta) = temp;
+		}	
+	}
+}
+
 int main()
 {
     node* root = NULL;
+    insert(root,2);
+    insert(root,5);
+	insert(root,6);
+	insert(root,4);
+	insert(root,3);
+	insert(root,7);
+    cout<<root->x << " "<< root->right->x << " " << root->right->right->x << endl;
+    remove2(root,2);
+    cout<<root->x << " "<< root->right->x << " " << root->right->right->x << endl;
     // insert
 
     return 0;
